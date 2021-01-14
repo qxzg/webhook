735efd356e58b52e2d71b7fa9d3da5116af9b267
qxzg <out@qxzg.xyz>
2021-01-14 10:11:14 +0800

    webhook

---
+++ web.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify
+from gevent import pywsgi
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkrds.request.v20140815.ModifySecurityIpsRequest import ModifySecurityIpsRequest
+import logging
+
+app = Flask(__name__)
+Token = ''
+AccessKey_ID = ""
+AccessKey_Secret = ""
+Rds_ID = "rm-"
+
+logging.basicConfig(level=logging.DEBUG, filename="log/stdout.log", filemode="a")
+
+def set_rds_security_ips(security_ips):
+    client = AcsClient(AccessKey_ID, AccessKey_Secret, 'cn-beijing')
+
+    request = ModifySecurityIpsRequest()
+    request.set_accept_format('json')
+
+    request.set_DBInstanceId(Rds_ID)
+    request.set_SecurityIps(security_ips)
+    request.set_DBInstanceIPArrayName("outside")
+
+    response = client.do_action_with_exception(request)
+    logging.info(str(response, encoding='utf-8'))
+
+@app.route('/hook', methods=['POST'])
+def post_data():
+    """
+    """
+    post_data = request.json
+    logging.info(post_data['msgTYPE'])
+    try:
+        if request.headers['X-Token'] != Token:
+            logging.error("token认证无效")
+            return "token认证无效", 401
+    except:
+        return "请求失败", 401
+    if post_data['msgTYPE'] == "ifUP":
+        wan_ip = post_data['netSTATE'][0]['pubIPv4']
+    elif post_data['msgTYPE'] == "manuINFO" or post_data['msgTYPE'] == "cronINFO":
+        wan_ip = post_data['netINFO']['WAN'][0]['pubIPv4']
+    else:
+        logging.error("IP更新失败")
+        return "IP更新失败", 401
+    set_rds_security_ips(wan_ip)
+    return jsonify({"status": 200})
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=13283, debug=False)
+    #server = pywsgi.WSGIServer(('0.0.0.0', 8989), app)
+    #server.serve_forever()
